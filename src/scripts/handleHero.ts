71577884e80fde2161ed8d985743e54d90de4b2c
Jordy756 <jordycastro1756@gmail.com>
2025-05-16 20:13:37 -0600

    Add HeroIntroduction and IngredientCard components
    - Implemented HeroIntroduction component with styled section for introductory text.
    - Created IngredientCard component to display ingredient details including name, quantity, description, and image.
    - Added animations and responsive styles to enhance visual appeal.

--- src/scripts/handleHero.ts
+++ src/scripts/handleHero.ts
@@ -1,15 +1,28 @@
 import { initLenis } from "@utils/lenis.ts";
+import { SplitText } from "gsap/SplitText";
 
 const { gsap, ScrollTrigger } = initLenis();
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
+await document.fonts.ready;
+
+const header = document.querySelector("#header");
 const heroSection = document.querySelector("#hero") as HTMLElement;
 const heroVideo = heroSection.querySelector("video") as HTMLVideoElement;
 const heroTitle = heroSection.querySelector("h2") as HTMLHeadingElement;
 const heroSubtitle = heroSection.querySelector("h3") as HTMLHeadingElement;
 const corners = heroSection.querySelectorAll(".corner__decoration") as NodeListOf<HTMLElement>;
 const newsletterForm = heroSection.querySelector(".slide__in-left") as HTMLElement;
+const heroIntroduction = heroSection.querySelector("#introduction") as HTMLElement;
+const paragraphs = heroIntroduction.querySelectorAll("div > p");
+
+const split = SplitText.create(paragraphs, { type: "words", aria: "hidden" });
+
+gsap.set(split.words, {
+    opacity: 0,
+    transform: "translateX(-100%)",
+});
 
 const heroToHistoryTl = gsap.timeline({
     scrollTrigger: {
@@ -24,6 +37,16 @@ const heroToHistoryTl = gsap.timeline({
 
 heroToHistoryTl
     .to(
+        header,
+        {
+            top: "0",
+            left: "1rem",
+            width: "calc(100% - 2rem)",
+            duration: 0.5,
+        },
+        0
+    )
+    .to(
         heroVideo,
         {
             scale: 1.3,
@@ -33,20 +56,20 @@ heroToHistoryTl
         0
     )
     .to(
-        heroSection.querySelector("article"),
+        heroTitle,
         {
-            y: "5%",
-            duration: 0.4,
+            scale: 200,
+            duration: 3,
+            transform: "translate(-550%, 1500%)",
         },
         0
     )
     .to(
         heroTitle,
         {
-            fontSize: "250px",
-            y: "25vh",
+            // y: "25vh",
             opacity: 0,
-            duration: 0.5,
+            duration: 1,
         },
         0
     )
@@ -62,6 +85,7 @@ heroToHistoryTl
     .to(
         newsletterForm,
         {
+            x: "100%",
             opacity: 0,
             duration: 0.3,
         },
@@ -73,6 +97,34 @@ heroToHistoryTl
             scale: 5,
             opacity: 0,
             duration: 0.3,
+        },
+        0
+    )
+    .to(
+        heroIntroduction,
+        {
+            opacity: 1,
+            left: "0%",
+            duration: 3,
+        },
+        0
+    )
+    .to(
+        split.words,
+        {
+            transform: "translateX(0)",
+            duration: 2.5,
+            stagger: 0.1,
+        },
+        0
+    )
+    .to(
+        split.words,
+        {
+            opacity: 1,
+            duration: 3,
+            ease: "sine.out",
+            stagger: 0.1,
         },
         0
     );
--- src/scripts/handleHistory.ts
+++ src/scripts/handleHistory.ts
@@ -1,30 +1,53 @@
-import { SplitText } from "gsap/SplitText";
-import { initLenis } from "@utils/lenis.ts";
+// import { SplitText } from "gsap/SplitText";
+// import { initLenis } from "@utils/lenis.ts";
 
-const { gsap, ScrollTrigger } = initLenis();
+// const { gsap, ScrollTrigger } = initLenis();
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+// gsap.registerPlugin(ScrollTrigger, SplitText);
 
-document.fonts.ready.then(() => {
-    gsap.set(".history > div", { opacity: 1 });
+// (gsap.utils.toArray(".history") as HTMLElement[]).forEach(async (section) => {
+//     await document.fonts.ready;
 
-    const historySections = document.querySelectorAll(".history");
+//     const paragraphs = section.querySelectorAll("div > p");
+//     let split = SplitText.create(paragraphs, { type: "words", aria: "hidden" });
 
-    historySections.forEach((section) => {
-        const paragraphs = section.querySelectorAll("div > p");
-        let split = SplitText.create(paragraphs, { type: "words", aria: "hidden" });
+//     gsap.from(split.words, {
+//         opacity: 0,
+//         duration: 1.5,
+//         ease: "sine.out",
+//         stagger: 0.1,
+//         scrollTrigger: {
+//             trigger: section,
+//             start: "top 70%",
+//             end: "bottom 80%",
+//             scrub: true,
+//         },
+//     });
 
-        gsap.from(split.words, {
-            opacity: 0,
-            duration: 1.5,
-            ease: "sine.out",
-            stagger: 0.1,
-            scrollTrigger: {
-                trigger: section,
-                start: "top 70%",
-                end: "bottom 80%",
-                scrub: true,
-            },
-        });
-    });
-});
+//     const heroToHistoryTl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: heroSection,
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: 1,
+//             pin: true,
+//             pinSpacing: true,
+//         },
+//     });
+
+//     gsap.to(section, {
+//         opacity: 1,
+//         width: "100%",
+//         height: "100%",
+//         scrollTrigger: {
+//             trigger: section,
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: 1,
+//             pin: true,
+//             pinSpacing: true,
+//         },
+//     });
+// });
+
+
