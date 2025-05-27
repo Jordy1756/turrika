e480db2d270b40ff1575ee1ee1d38437225b2926
Jordy756 <jordycastro1756@gmail.com>
2025-05-27 15:58:44 -0600

    Update API base URL: comment out local API URL and set production URL for user API access.

--- src/constants/apiConstant.ts
+++ src/constants/apiConstant.ts
@@ -1,2 +1,4 @@
-const API_URL_BASE = "http://localhost:3000/api";
+// const API_URL_BASE = "http://localhost:3000/api";
+const API_URL_BASE = "https://turrika-api.vercel.app";
+
 export const USER_API_URL = `${API_URL_BASE}/user`;
